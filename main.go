f3749e1e875703bff57a53e6b468f07ca94bb29b
Gita Perdani <gitaperdani08@gmail.com>
2018-04-21 16:22:14 +0700

    halaman profil

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Halaman Beranda")
 }
+func profil(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Halaman Profil")
+}
 
 func main(){
 http.HandleFunc("/", home)
